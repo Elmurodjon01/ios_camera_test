10d254bd5f0aaf9e8372b3bd00f316cae736210e
Elmurodjon01 <abdulajonov01@gmail.com>
2023-06-30 18:09:21 +0900

    test screen 2 has been added

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,6 +1,7 @@
 import 'package:cameratest/cameraTestPage.dart';
+import 'package:cameratest/otherCameraTest.dart';
 import 'package:flutter/material.dart';
-
+//dd
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,13 +13,37 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: IconButton(
-          onPressed: () => Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const CameraTestPage())),
-          icon: const Icon(
-            Icons.camera_alt_outlined,
-            size: 40,
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            MaterialButton(onPressed:   (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> const CameraTestPage()));
+            },
+            child:  Container(
+              height: 50,
+              width: 100,
+              color: Colors.red,
+              child: const Row(
+                children: [
+                  Text('Camera test 1')
+                ],
+              ),
+            ),),
+            MaterialButton(onPressed:   (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> DistanceTrackingPage()));
+            },
+            child:  Container(
+              height: 50,
+              width: 100,
+              color: Colors.blue,
+              child: const Row(
+                children: [
+                  Text('Camera test 2')
+                ],
+              ),
+            ),),
+          ],
         ),
       ),
     );
